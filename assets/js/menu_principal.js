34b2569873afe43a4870f766d0c9c973291e7ccb
Christophe Van Maercke <vanmaerckechri@gmail.com>
2017-12-25 22:41:05 +0100

    JS - options par defaut

---
+++ assets/js/menu_principal.js
@@ -0,0 +1,10 @@
+var couleurDifAutoriser = false;
+var couleurDifNbr = 6;
+var tentativesNbr = 10;
+
+function optionsDefaut()
+{
+	document.getElementById('couleurDif').checked = false;
+	document.getElementById('couleurNbr6').checked = true;
+	document.getElementById('tentatives10').checked = true;
+}
