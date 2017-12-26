5d14dd4765f5025b559085b78a4c0f0489ea9307
Christophe Van Maercke <vanmaerckechri@gmail.com>
2017-12-26 06:23:03 +0100

    Choix Pions

--- assets/js/menu_principal.js
+++ assets/js/menu_principal.js
@@ -40,11 +40,68 @@ function nouvellePartie()
 /* Création de la table de jeu */
 function placementCases()
 {
-	let distributionCaseId = 0;
+	let distributionCaseRowId = 0;
 	let distributionResultatRowId = 0;
 	let distributionResultatId = 0;
 	for (i = 0; i < tentativesNbr; i++)
 	{
-		document.getElementById('tableCases').innerHTML += '<div class="casesRow"><span id="case'+(distributionCaseId++)+'" class="case"></span><span id="case'+(distributionCaseId++)+'" class="case"></span><span id="case'+(distributionCaseId++)+'" class="case"></span><span id="case'+(distributionCaseId++)+'" class="case"></span><span id="caseResultatRow'+(distributionResultatRowId++)+'" class="case caseResultatRow"><span id="caseResultat'+(distributionResultatId++)+'" class="caseResultat"></span><span id="caseResultat'+(distributionResultatId++)+'" class="caseResultat"></span><span id="caseResultat'+(distributionResultatId++)+'" class="caseResultat"></span><span id="caseResultat'+(distributionResultatId++)+'" class="caseResultat"></span></span></div>';
+		document.getElementById('tableCases').innerHTML += '<div id="caseRow'+(distributionCaseRowId++)+'" class="casesRow"><span id="case" class="case"></span><span id="case" class="case"></span><span id="case" class="case"></span><span id="case" class="case"></span><span id="caseResultatRow'+(distributionResultatRowId++)+'" class="case caseResultatRow"><span id="caseResultat'+(distributionResultatId++)+'" class="caseResultat"></span><span id="caseResultat'+(distributionResultatId++)+'" class="caseResultat"></span><span id="caseResultat'+(distributionResultatId++)+'" class="caseResultat"></span><span id="caseResultat'+(distributionResultatId++)+'" class="caseResultat"></span></span></div>';
 	}
+}
+
+var caseRowAct = 0;
+
+
+function deposerPion()
+{
+	let rowActuelle = document.getElementById('caseRow'+caseRowAct);
+	rowActuelle.onmousemove = function()
+	{
+		rowActuelle.childNodes[1].style.backgroundColor = "red";
+	} 
+	pionEnDeplacement = false;
+}
+
+var pionEnDeplacement = false;
+
+
+/* Déplacement du pion choisi - drag and drop*/
+
+
+
+
+function choisirPion(event, couleur)
+{
+	if (pionEnDeplacement == false)
+	{
+		document.getElementById('pions').innerHTML += '<div id="couleurSelect" class="pion '+couleur+'"></div>';
+		couleurSelect.style.position = 'absolute';
+		couleurSelect.style.zIndex = 1000;
+
+		moveAt(event.pageX, event.pageY);
+
+		/* Bouge l'élément quand la souris bouge */
+		function moveAt(pageX, pageY)
+		{
+			if (document.getElementById('couleurSelect'))
+			{
+				couleurSelect.style.left = pageX - couleurSelect.offsetWidth / 2 + 'px';
+			    couleurSelect.style.top = pageY - couleurSelect.offsetHeight / 2 + 'px';
+			}
+		}
+		function onMouseMove(event)
+		{
+			moveAt(event.pageX, event.pageY);
+		}
+		document.addEventListener('mousemove', onMouseMove);
+
+		/* Lache l'élément lorsque le clique de la souris est relaché */
+		couleurSelect.onmouseup = function()
+		{
+			deposerPion();
+		    couleurSelect.onmouseup = null;
+			document.getElementById("couleurSelect").remove();
+		};
+	}
+
 }
