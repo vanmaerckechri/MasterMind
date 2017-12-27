5fb3cab99502d24b878730aa4cac10f077b61734
Christophe Van Maercke <vanmaerckechri@gmail.com>
2017-12-27 21:27:49 +0100

    comparaison proposition et code secret

--- assets/js/menu_principal.js
+++ assets/js/menu_principal.js
@@ -2,6 +2,8 @@ var couleurDifAutoriser = false;
 var couleurDifNbr = 6;
 var tentativesNbr = 10;
 var couleurDif = ['yellow', 'blue', 'red', 'green', 'white', 'black', 'maroon', 'purple', 'orange', 'pink'];
+var caseRowAct = 0;
+var colNbr = 4;
 
 function optionsDefaut()
 {
@@ -36,6 +38,7 @@ function nouvellePartie()
     document.getElementById('ecranMenuPrincipal').style.display = "none";
     document.getElementById('ecranJeu').style.display = "block";
     placementCases();
+    combiSecreteInstall();
 }
 
 /* Création de la table de jeu */
@@ -43,10 +46,9 @@ function placementCases()
 {
 	let distributionCaseRowId = 0;
 	let distributionResultatRowId = 0;
-	let distributionResultatId = 0;
 	for (i = 0; i < tentativesNbr; i++)
 	{
-		document.getElementById('tableCases').innerHTML += '<div id="caseRow'+(distributionCaseRowId++)+'" class="casesRow"><span class="case"></span><span class="case"></span><span class="case"></span><span class="case"></span><span id="caseResultatRow'+(distributionResultatRowId++)+'" class="case caseResultatRow"><span id="caseResultat'+(distributionResultatId++)+'" class="caseResultat"></span><span id="caseResultat'+(distributionResultatId++)+'" class="caseResultat"></span><span id="caseResultat'+(distributionResultatId++)+'" class="caseResultat"></span><span id="caseResultat'+(distributionResultatId++)+'" class="caseResultat"></span></span></div>';
+		document.getElementById('tableCases').innerHTML += '<div id="caseRow'+(distributionCaseRowId++)+'" class="casesRow"><span class="case"></span><span class="case"></span><span class="case"></span><span class="case"></span><span id="caseResultatRow'+(distributionResultatRowId++)+'" class="case caseResultatRow"><span class="caseResultat"></span><span class="caseResultat"></span><span class="caseResultat"></span><span class="caseResultat"></span></span></div>';
 	}
 	for (i = 0; i < couleurDifNbr; i++)
 	{
@@ -54,7 +56,6 @@ function placementCases()
 		couleur = couleurDif[i];
 		document.getElementById('pions').innerHTML += '<span class="pion '+couleur+'" onmousedown="choisirPion(event,\''+couleur+'\');"></span>';
 	}
-	combiSecreteInstall();
 }
 /* Construction Combinaison Secrete */
 function combiSecreteInstall()
@@ -64,93 +65,153 @@ function combiSecreteInstall()
 	/* si l'utilisation d'une même couleur n'est pas autorisée */
 	if (couleurDifAutoriser == false)
 	{
-		for (i = 0; i < 4; i++)
+		for (i = 0; i < colNbr; i++)
 		{
 			randomNum = Math.floor(Math.random()*couleurNbrRestantes);
-			document.getElementById('rowSecrete').innerHTML += '<span class="case '+couleurDifRestantes[randomNum]+'"></span>';
+			document.getElementById('rowSecrete').innerHTML += '<span class="case" style="background-color: '+couleurDifRestantes[randomNum]+';"></span>';
 			couleurDifRestantes.splice(randomNum, 1);
 			couleurNbrRestantes--;
 		}
 	}
+	/* si l'utilisation d'une même couleur est autorisée */
 	else
 	{
-		for (i = 0; i < 4; i++)
+		for (i = 0; i < colNbr; i++)
 		{
-			randomNum = Math.floor(Math.random()*couleurNbrRestantes);
-			document.getElementById('rowSecrete').innerHTML += '<span class="case '+couleurDifRestantes[randomNum]+'"></span>';
+			let randomNum = Math.floor(Math.random()*couleurNbrRestantes);
+			document.getElementById('rowSecrete').innerHTML += '<span class="case" style="background-color: '+couleurDifRestantes[randomNum]+';"></span>';
 		}
 	}
 }
 
-
-
-
-var caseRowAct = 0;
-
-
+/* Déplacement du pion choisi - drag and drop*/
 function desactiveOnMouseOver()
 {
 	let rowActuelle = document.getElementById('caseRow'+caseRowAct);
-	for (i = 0; i < 4; i++)
+	for (i = 0; i < colNbr; i++)
 	{
 		rowActuelle.childNodes[i].onmouseover = null;
 	}	
 }
-
 function deposerPion(couleur)
 {
 	let rowActuelle = document.getElementById('caseRow'+caseRowAct);
-	for (i = 0; i < 4; i++)
+	for (i = 0; i < colNbr; i++)
 	{
 		rowActuelle.childNodes[i].onmouseover = function()
 		{
 			this.style.backgroundColor = couleur;
 		}
 	}
-	setTimeout(desactiveOnMouseOver, 100);
-	pionEnDeplacement = false;
+	setTimeout(desactiveOnMouseOver, 10);
+	setTimeout(verifRowActuComplete, 20);
+
 }
-
-var pionEnDeplacement = false;
-
-
-/* Déplacement du pion choisi - drag and drop*/
-
-
-
-
 function choisirPion(event, couleur)
 {
-	if (pionEnDeplacement == false)
+	document.getElementById('pions').innerHTML += '<div id="couleurSelect" class="pion '+couleur+'"></div>';
+	couleurSelect.style.position = 'absolute';
+	couleurSelect.style.zIndex = 1000;
+	moveAt(event.pageX, event.pageY);
+	/* Bouge l'élément quand la souris bouge */
+	function moveAt(pageX, pageY)
 	{
-		document.getElementById('pions').innerHTML += '<div id="couleurSelect" class="pion '+couleur+'"></div>';
-		couleurSelect.style.position = 'absolute';
-		couleurSelect.style.zIndex = 1000;
-
-		moveAt(event.pageX, event.pageY);
-
-		/* Bouge l'élément quand la souris bouge */
-		function moveAt(pageX, pageY)
+		if (document.getElementById('couleurSelect'))
 		{
-			if (document.getElementById('couleurSelect'))
+			couleurSelect.style.left = pageX - couleurSelect.offsetWidth / 2 + 'px';
+			couleurSelect.style.top = pageY - couleurSelect.offsetHeight / 2 + 'px';
+		}
+	}
+	function onMouseMove(event)
+	{
+		moveAt(event.pageX, event.pageY);
+	}
+	document.addEventListener('mousemove', onMouseMove);
+	/* Lache l'élément lorsque le clique de la souris est relaché */
+	couleurSelect.onmouseup = function()
+	{
+		couleurSelect.onmouseup = null;
+		document.getElementById("couleurSelect").remove();
+		deposerPion(couleur);
+	};
+}
+/* Verifie si la rangée actuelle est complète */
+function verifRowActuComplete()
+{
+	let rowActuelle = document.getElementById('caseRow'+caseRowAct);
+	let casesRempliesNbr = 0;
+	for (i = 0; i < colNbr; i++)
+	{
+		if (rowActuelle.childNodes[i].style.backgroundColor)
+		{
+			casesRempliesNbr++
+			if (casesRempliesNbr == colNbr)
 			{
-				couleurSelect.style.left = pageX - couleurSelect.offsetWidth / 2 + 'px';
-			    couleurSelect.style.top = pageY - couleurSelect.offsetHeight / 2 + 'px';
+				comparer();
 			}
 		}
-		function onMouseMove(event)
-		{
-			moveAt(event.pageX, event.pageY);
-		}
-		document.addEventListener('mousemove', onMouseMove);
-
-		/* Lache l'élément lorsque le clique de la souris est relaché */
-		couleurSelect.onmouseup = function()
-		{
-			couleurSelect.onmouseup = null;
-			document.getElementById("couleurSelect").remove();
-			deposerPion(couleur);
-		};
 	}
+}
 
+function comparer()
+{
+	let rowActuelle = document.getElementById('caseRow'+caseRowAct);
+	let rowSecreteID = document.getElementById('rowSecrete');
+	let resultatComparaison = [];
+	for (i = 0; i < colNbr; i++)
+	{
+		for (j = 0; j < colNbr; j++)
+		{
+			if (rowActuelle.childNodes[i].style.backgroundColor == rowSecreteID.childNodes[j].style.backgroundColor)
+			{
+				/* s'ils sont à la même place */
+				if (i == j)
+				{
+					resultatComparaison[i] = 'parfait';
+					break;
+				}
+				/* s'ils ne sont pas à la même place */
+				else
+				{
+					resultatComparaison[i] = 'bon';
+					break;
+				}
+			}
+			else
+			{
+					resultatComparaison[i] = 'mauvais';
+			}
+		}
+	}
+	indicesAfficher(resultatComparaison[0], resultatComparaison[1],resultatComparaison[2],resultatComparaison[3]);
+}
+
+function indicesAfficher(ind1, ind2, ind3, ind4)
+{
+	/* mélange les indices */
+	let indicesTemp = [ind1, ind2, ind3, ind4];
+	let indices = [];
+	let NbrRestant = colNbr;
+	let randomNum;
+	for (i = 0; i < colNbr; i++)
+	{
+		randomNum = Math.floor(Math.random()*NbrRestant);
+		indices[i] = indicesTemp[randomNum];
+		indicesTemp.splice(randomNum, 1);
+		NbrRestant--;
+	}
+	/* affichage */
+	let rowResultatActuelle = document.getElementById('caseResultatRow'+caseRowAct);
+	for (i = 0; i < colNbr; i++)
+	{
+		if (indices[i] == 'parfait')
+		{
+			rowResultatActuelle.childNodes[i].style.backgroundColor = "red";
+		}
+		if (indices[i] == 'bon')
+		{
+			rowResultatActuelle.childNodes[i].style.backgroundColor = "green";
+		}
+	}
+	caseRowAct++;
 }
